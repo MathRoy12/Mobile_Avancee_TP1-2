d4e832712c36b410eb649d4f7528a71e28124612
Mathieu Roy <2234476@cegepmontpetit.ca>
2024-06-06 15:34:56 -0400

    FCT Inscription/Creation Design
    Ajout de l'interface pour la page Creation et Design

---
+++ lib/pages/connection_page.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_avancee_tp1_2/pages/inscription_page.dart';
+
+class ConnectionPage extends StatefulWidget {
+  const ConnectionPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<ConnectionPage> createState() => _ConnectionPageState();
+}
+
+class _ConnectionPageState extends State<ConnectionPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String _name = ''; // Variable to store the entered name
+  String _password = '';
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+
+      print('Name: $_name');
+      print('Password: $_password');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Connection"),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      label: Text("Username"), border: OutlineInputBorder()),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Veuillez écrire votre nom d'utilisateur";
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _name = value!;
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: TextFormField(
+                  obscureText: true,
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  decoration: const InputDecoration(
+                      label: Text("Password"), border: OutlineInputBorder()),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Veuillez écrire votre mot de passe";
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _password = value!;
+                  },
+                ),
+              ),
+              const SizedBox(height: 20),
+              MaterialButton(
+                onPressed: _submitForm,
+                color: Colors.blue,
+                child: const Text(
+                  'Connexion',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              MaterialButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const InscriptionPage(title: "Inscription")));
+                },
+                color: Colors.blue,
+                child: const Text(
+                  'Inscription',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/inscription_page.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+
+class InscriptionPage extends StatefulWidget {
+  const InscriptionPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<InscriptionPage> createState() => _InscriptionPageState();
+}
+
+class _InscriptionPageState extends State<InscriptionPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String _name = ''; // Variable to store the entered name
+  String _password = '';
+  String _confirmPassword = '';
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+
+      print('Name: $_name');
+      print('Password: $_password');
+      print('Confirm password: $_confirmPassword');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Inscription"),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      label: Text("Username"), border: OutlineInputBorder()),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Veuillez écrire votre nom d'utilisateur";
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _name = value!;
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: TextFormField(
+                  obscureText: true,
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  decoration: const InputDecoration(
+                      label: Text("Password"), border: OutlineInputBorder()),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Veuillez écrire votre mot de passe";
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _password = value!;
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: TextFormField(
+                  obscureText: true,
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  decoration: const InputDecoration(
+                      label: Text("Confirm password"), border: OutlineInputBorder()),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Veuillez écrire votre mot de passe";
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _confirmPassword = value!;
+                  },
+                ),
+              ),
+              const SizedBox(height: 20),
+              MaterialButton(
+                onPressed: _submitForm,
+                color: Colors.blue,
+                child: const Text(
+                  'Inscription',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              MaterialButton(
+                onPressed: () {Navigator.of(context).pop();},
+                color: Colors.blue,
+                child: const Text(
+                  'Connection',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
